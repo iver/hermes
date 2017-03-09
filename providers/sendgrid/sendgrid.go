026b88743714c1c6bb122e4c2c778dd01effbebd
MauricioCD <mau.cdr.19@gmail.com>
2017-03-08 23:46:21 -0600

    add support multi recipient sendgrid

--- providers/sendgrid/sendgrid.go
+++ providers/sendgrid/sendgrid.go
@@ -2,32 +2,49 @@ package providers
 
 import (
 	"os"
+		"fmt"
+		"log"
 	"github.com/ivan-iver/hermes/models"
-	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"github.com/sendgrid/sendgrid-go"
 )
 
 type Sendgrid struct {
 }
 
 // SendEmail method with mailgun provider
-func (p *Sendgrid) SendEmail(email models.Email) (err error) {
+func (p *Sendgrid) SendEmail(email models.Email) (er error) {
 
-	APIKey :=   os.Getenv("SENDGRID_API_KEY")
-
+	APIKey := os.Getenv("SENDGRID_API_KEY")
 	from := mail.NewEmail(email.SenderName, email.SenderEmail)
 	subject := email.Subject
-	to := mail.NewEmail("You", email.Recipients[0])
-	content := mail.NewContent("text/plain", email.Content)
-	m := mail.NewV3MailInit(from, subject, to, content)
+	content := mail.NewContent("text/plain", email.Template)
 
+	recipients := []*mail.Email{}
+
+	for _, email := range email.Recipients {
+        eml := mail.Email{Address: email}
+		recipients = append(recipients, &eml )
+	}
+
+	m := mail.NewV3Mail()
+	m.SetFrom(from)
+	m.Subject = subject
+	ps := mail.NewPersonalization()
+    ps.AddTos(recipients...)
+	m.AddPersonalizations(ps)
+	m.AddContent(content)
 	request := sendgrid.GetRequest(APIKey, "/v3/mail/send", "https://api.sendgrid.com")
 	request.Method = "POST"
 	request.Body = mail.GetRequestBody(m)
-	_, err = sendgrid.API(request)
+	response, err := sendgrid.API(request)
 	if err != nil {
+        log.Println(err)
 		return err
-	}
-
+    } else {
+        fmt.Println(response.StatusCode)
+        fmt.Println(response.Body)
+        fmt.Println(response.Headers)
+    }
 	return
 }
--- test/providers/sendgrid_test.go
+++ test/providers/sendgrid_test.go
@@ -11,20 +11,21 @@ import (
 func TestSendGridSendEmail(t *testing.T) {
 	var provider = p.Sendgrid{}
 	userEmail := os.Getenv("SENDGRID_USER")
-	template := "<h1>notifik</h1><br><h3>endgrid</h3>"
-	rec := []string{userEmail}
+	template := "<div><h1>Hola desde sendgrid<h1><h4>Template desde hermes</h4></div>"
+	recipients := []string{userEmail, "mau16@ciencias.unam.mx"}
+
 	email := models.Email{
 		Subject:     "Hola desde sendgrid",
-		SenderEmail: "mau.cdr.19@gmail.com",
+		SenderEmail: "person@hermes.mx",
 		SenderName:  "Alguien",
-		Recipients:  rec,
-		Content:     template,
+		Recipients:  recipients,
+		Template:    template,
 	}
-
+   
 	if err := provider.SendEmail(email); err != nil {
-		t.Error("Ya valio: ", err)
+		t.Error("hermes: error in provider", err)
 	}
-
+   
 	return
 
 }
